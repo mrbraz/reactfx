3cdd80c5f2b8dd901edd7eeffda6c30b07f786f2
mrbraz <anderson.braz@gmail.com>
2015-06-25 11:37:27 -0300

    Async Events

--- src/main/java/reactivefx/infra/event/EventBus.java
+++ src/main/java/reactivefx/infra/event/EventBus.java
@@ -4,12 +4,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
 
+import javafx.application.Platform;
+
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class EventBus {
   private Map<Class<? extends Event>, List<Channel<? extends Event>>> handlers = new HashMap<>();
+  
+  private @Inject ExecutorService executor;
+  
+  public <E extends Event> void asyncDispatch(E event){
+    Runnable command = new Runnable() {
+      @Override
+      public void run() {
+        EventBus.this.dispatch(event);
+      }
+    };
+    this.executor.execute(command);
+  }
+  
+  public <E extends Event> void fxDispatch(E event){
+    Runnable command = new Runnable() {
+      @Override
+      public void run() {
+        EventBus.this.dispatch(event);
+      }
+    };
+    Platform.runLater(command);
+  }
   
   public void register(Channel<? extends Event> channel) {
     if (!this.handlers.containsKey(channel.type())) {
